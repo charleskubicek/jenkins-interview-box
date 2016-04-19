438ff7f0e9f0112a5025fd06ca180ad22ea4136a
Pete Smith <pete.smith@digital.hmrc.gov.uk>
2016-04-19 16:34:48 +0100

    Provision script now also includes SBT and oracle java 8

--- provision.sh
+++ provision.sh
@@ -1,5 +1,15 @@
+sudo add-apt-repository -y ppa:webupd8team/java
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
+
 wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update 
-sudo apt-get install -y openjdk-7-jre-headless
+
+sudo apt-get update
+sudo apt-get -y upgrade
+
+sudo apt-get -y install oracle-java8-installer
+sudo apt-get install -y sbt
 sudo apt-get install -y jenkins
